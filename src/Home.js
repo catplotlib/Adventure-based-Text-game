5f1aea7b92a99b68353440e1b552844a5c61155e
catplotlib <43911345+Puja2402@users.noreply.github.com>
2021-07-18 16:29:50 +0530

    changed effect to full page

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
+    <div className="Home">
       <img className="Zork" src={wel} alt="logo" />
       <Link style={{ textDecoration: "none"}} to="/game">
         <button className="btn">Proceed to game</button>
       </Link>
+      </div>
     </>
   );
 }
